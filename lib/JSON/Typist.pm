34a37648e72fa31feafc1251ddc22e0dfc404e20
Ricardo Signes <rjbs@cpan.org>
2016-03-22 18:25:41 -0400

    docs and tests

--- lib/JSON/Typist.pm
+++ lib/JSON/Typist.pm
@@ -2,6 +2,53 @@ use strict;
 use warnings;
 
 package JSON::Typist;
+# ABSTRACT: replace mushy strings and numbers with rigidly typed replacements
+
+=head1 OVERVIEW
+
+JSON is super useful and everybody loves it.  Woo!  Go JSON!  Good job!
+
+In Perl, though, it's a bit of a pain sometimes.  In Perl, strings and numbers
+mush all together and you're often not sure which you have.  Did the C<5> in
+your C<$x> come from C<{"x":5}> or C<{"x":"5"}>?  By the time you're checking,
+you very well may not know.
+
+Often, that's just fine, because it doesn't matter inside your Perl program,
+where numericality and stringicity are determined by operators, not values.
+Other times, you need to know.  You might using JSON for interchange with a
+system that needs its types in its values.  JSON::Typist is meant for this
+problem.
+
+L<JSON> (in its many variant forms) always returns numbers and strings in
+distinguishable forms, but the distinction can be lost as the variables are
+used.  (That's just a weird-o Perl problem.)  JSON::Typist is meant to take the
+result of JSON-decoding I<immediately> before you use it for anything else.  It
+replaces numbers and strings with objects.  These objects can be used like
+numbers and strings, and JSON will convert them to the right type if
+C<convert_blessed> is enabled.
+
+=head1 SYNOPSIS
+
+  my $content = q<{ "number": 5, "string": "5" }>;
+
+  my $json = JSON->new->convert_blessed->canonical;
+
+  my $payload = $json->decode( $content );
+  my $typed   = JSON::Typist->new->apply_types( $payload );
+
+  $typed->{string}->isa('JSON::Typist::String'); #true
+  $typed->{number}->isa('JSON::Typist::Number'); # true
+
+  say 0 + $payload->{string}; # prints 5
+  say "$payload->{number}";   # prints 5
+
+  say 0 + $typed->{string};   # prints 5
+  say "$typed->{number}";     # prints 5
+
+  say $json->encode($payload);
+  say $json->encode($typed);
+
+=cut
 
 use B ();
 use Params::Util qw(_HASH0 _ARRAY0);
@@ -11,17 +58,46 @@ use Scalar::Util qw(blessed);
   package JSON::Typist::Number;
   use overload '0+' => sub { ${ $_[0] } }, fallback => 1;
   sub new { my $x = $_[1]; bless \$x, $_[0] }
-  sub TO_JSON { 0 + ${$_[1]} }
+  sub TO_JSON { 0 + ${$_[0]} }
 }
 
 {
   package JSON::Typist::String;
   use overload '""' => sub { ${ $_[0] } }, fallback => 1;
   sub new { my $x = $_[1]; bless \$x, $_[0] }
-  sub TO_JSON { "${$_[1]}" }
+  sub TO_JSON { "${$_[0]}" }
 }
 
-sub type_annotate {
+=method new
+
+  my $typist = JSON::Typist->new( \%arg );
+
+This returns a new JSON::Typist.  There are no valid arguments to C<new> yet.
+
+=cut
+
+sub new {
+  my ($class) = @_;
+
+  bless {}, $class;
+}
+
+=method apply_types
+
+  my $typed = $json_typist->apply_types( $data );
+
+This returns a new variables that deeply copies the input C<$data>, replacing
+numbers and strings with objects.  The logic used to test for number-or-string
+is subject to change, but is meant to track the logic used by JSON.pm and
+related JSON libraries.  The behavior on weird-o scalars like globs I<is
+undefined>.
+
+Note that property names, which becomes hash keys, do not become objects.  Hash
+keys are always strings.
+
+=cut
+
+sub apply_types {
   my ($self, $data) = @_;
 
   return $data unless defined $data;
@@ -35,14 +111,44 @@ sub type_annotate {
     }
   }
 
-  return [ map {; $self->type_annotate($_) } @$data ] if _ARRAY0($data);
+  return [ map {; $self->apply_types($_) } @$data ] if _ARRAY0($data);
 
-  return { map {; $_ => $self->type_annotate($data->{$_}) } keys %$data }
+  return { map {; $_ => $self->apply_types($data->{$_}) } keys %$data }
     if _HASH0($data);
 
   return JSON::Typist::Number->new($data)
     if blessed $data
     && ($data->isa('Math::BigInt') || $data->isa('Math::BigFloat'));
+
+  return $data;
+}
+
+=method strip_types
+
+  my $untyped = $json_typist->strip_types;
+
+This method deeply copies its input, replacing number and string objects with
+simple scalars that should become the proper JSON type.  Using this method
+should not be needed if your JSON decoder has C<convert_blessed> enabled.
+
+=cut
+
+sub strip_types {
+  my ($self, $data) = @_;
+
+  return $data unless defined $data;
+
+  if (blessed $data) {
+    return $$data if $data->isa('JSON::Typist::Number')
+                  or $data->isa('JSON::Typist::String');
+
+    return $data;
+  }
+
+  return [ map {; $self->strip_types($_) } @$data ] if _ARRAY0($data);
+
+  return { map {; $_ => $self->strip_types($data->{$_}) } keys %$data }
+    if _HASH0($data);
 
   return $data;
 }
--- t/wrapped.t
+++ t/wrapped.t
@@ -1,17 +1,36 @@
 use strict;
 use warnings;
-use Data::Dumper;
+use Test::More;
 use JSON::PP;
 use JSON::Typist;
 
-$Data::Dumper::Sortkeys = 1;
+my $content = q<{"number":5,"string":"5"}>;
 
-my $JSON = JSON::PP->new;
+my $json   = JSON::PP->new->convert_blessed->canonical;
+my $typist = JSON::Typist->new;
 
-my $json = q<{"num":123, "str":"this is a string"}>;
+my $payload  = $json->decode( $content );
+my $typed    = $typist->apply_types( $payload );
 
-warn Dumper({
-  input  => $json,
-  output => $JSON->decode($json),
-  typed  => JSON::Typist->type_annotate($JSON->decode($json)),
-});
+isa_ok( $typed->{string}, 'JSON::Typist::String', '$typed->{string}');
+isa_ok( $typed->{number}, 'JSON::Typist::Number', '$typed->{number}');
+
+my $sink;
+$sink = 0 + $payload->{string};
+$sink = "$payload->{number}";
+
+$sink = 0 + $typed->{string};
+$sink = "$typed->{number}";
+
+my $via_payload   = $json->encode($payload);
+my $via_typed     = $json->encode($typed);
+
+my $stripped      = $typist->strip_types($typed);
+my $via_stripped  = $json->encode($stripped);
+
+isnt($via_payload, $content, "once inspected, original won't round trip");
+
+is($via_typed, $content, "typed structure, inspected, does round trip");
+is($via_stripped, $content, "typed structure, stripped, also round trips");
+
+done_testing;
